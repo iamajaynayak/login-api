d0f7c7d88960c637c79983feb95d6c28c7ecd284
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2021-02-04 20:51:46 +0530

    fixed deprecated errors

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/account")
+  .connect("mongodb://localhost:27017/account", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("database is online");
   })

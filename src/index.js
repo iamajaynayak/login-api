29ac72a51a6092aa1675c8d0e68587b9fd81f5e1
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2021-02-05 21:14:46 +0530

    cors and body-parser added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
-require("./db/connection");
-const routes = require("./routes/accoutsRoute");
+const express = require("express"); // express
+const app = express(); // initialize express
+const cors = require("cors"); // cors
+const bodyParser = require("body-parser"); // body-parser
+require("./db/connection"); // database connection
+const routes = require("./routes/accoutsRoute"); // routes
 // const Account = require("./model/account");
-
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.listen(8080, () => console.log("server is online"));
